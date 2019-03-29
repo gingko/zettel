5884b4a0135b0f98c4e9a087cfa94efe24c15bba
Adriano Ferrari <adriano.ferrari@gmail.com>
2019-03-29 15:24:15 -0400

    Add before, after, map, mapCurrent to Deck

--- src/Deck.elm
+++ src/Deck.elm
@@ -1,4 +1,4 @@
-module Deck exposing (Deck, current, fromList, insert, isEmpty, next, previous, remove, select, sort, sortBy, toList)
+module Deck exposing (Deck, after, before, current, fromList, insert, isEmpty, map, mapCurrent, next, previous, remove, select, sort, sortBy, toList)
 
 import List.Zipper as LZ exposing (Zipper)
 import Types exposing (..)
@@ -6,6 +6,30 @@ import Types exposing (..)
 
 type Deck a
     = Deck (Maybe (Zipper a))
+
+
+fromList : List a -> Deck a
+fromList list =
+    Deck (LZ.fromList list)
+
+
+toList : Deck a -> List a
+toList (Deck mbz_) =
+    case mbz_ of
+        Just zipper ->
+            zipper |> LZ.toList
+
+        Nothing ->
+            []
+
+
+isEmpty : Deck a -> Bool
+isEmpty (Deck mbz_) =
+    mbz_ == Nothing
+
+
+
+-- MODIFIERS
 
 
 insert : a -> Deck a -> Deck a
@@ -42,56 +66,6 @@ remove ((Deck zip_) as deck) =
 
         Nothing ->
             deck
-
-
-current : Deck a -> Maybe a
-current (Deck mbz_) =
-    case mbz_ of
-        Nothing ->
-            Nothing
-
-        Just zip ->
-            Just (LZ.current zip)
-
-
-select : (a -> Bool) -> Deck a -> Deck a
-select predicate (Deck mbz_) =
-    case mbz_ of
-        Nothing ->
-            Deck Nothing
-
-        Just zip ->
-            zip
-                |> LZ.findFirst predicate
-                |> Deck
-
-
-next : Deck a -> Deck a
-next (Deck zip_) =
-    let
-        newZip_ =
-            zip_ |> Maybe.andThen LZ.next
-    in
-    case newZip_ of
-        Nothing ->
-            Deck zip_
-
-        Just newZip ->
-            Deck (Just newZip)
-
-
-previous : Deck a -> Deck a
-previous (Deck zip_) =
-    let
-        newZip_ =
-            zip_ |> Maybe.andThen LZ.previous
-    in
-    case newZip_ of
-        Nothing ->
-            Deck zip_
-
-        Just newZip ->
-            Deck (Just newZip)
 
 
 sort : Deck comparable -> Deck comparable
@@ -132,24 +106,86 @@ sortBy comp ((Deck zip_) as deck) =
             deck
 
 
-fromList : List a -> Deck a
-fromList list =
-    Deck (LZ.fromList list)
+map : (a -> b) -> Deck a -> Deck b
+map mapFn (Deck zip_) =
+    zip_
+        |> Maybe.map (LZ.map mapFn)
+        |> Deck
 
 
-toList : Deck a -> List a
-toList (Deck mbz_) =
+mapCurrent : (a -> a) -> Deck a -> Deck a
+mapCurrent mapFn (Deck zip_) =
+    zip_
+        |> Maybe.map (LZ.mapCurrent mapFn)
+        |> Deck
+
+
+
+-- ACCESSORS
+
+
+current : Deck a -> Maybe a
+current (Deck mbz_) =
     case mbz_ of
-        Just zipper ->
-            zipper |> LZ.toList
-
         Nothing ->
-            []
+            Nothing
+
+        Just zip ->
+            Just (LZ.current zip)
 
 
-isEmpty : Deck a -> Bool
-isEmpty (Deck mbz_) =
-    mbz_ == Nothing
+before : Deck a -> Maybe (List a)
+before (Deck zip_) =
+    Maybe.map LZ.before zip_
+
+
+after : Deck a -> Maybe (List a)
+after (Deck zip_) =
+    Maybe.map LZ.after zip_
+
+
+
+-- SELECTORS
+
+
+select : (a -> Bool) -> Deck a -> Deck a
+select predicate (Deck mbz_) =
+    case mbz_ of
+        Nothing ->
+            Deck Nothing
+
+        Just zip ->
+            zip
+                |> LZ.findFirst predicate
+                |> Deck
+
+
+next : Deck a -> Deck a
+next (Deck zip_) =
+    let
+        newZip_ =
+            zip_ |> Maybe.andThen LZ.next
+    in
+    case newZip_ of
+        Nothing ->
+            Deck zip_
+
+        Just newZip ->
+            Deck (Just newZip)
+
+
+previous : Deck a -> Deck a
+previous (Deck zip_) =
+    let
+        newZip_ =
+            zip_ |> Maybe.andThen LZ.previous
+    in
+    case newZip_ of
+        Nothing ->
+            Deck zip_
+
+        Just newZip ->
+            Deck (Just newZip)
 
 
 
